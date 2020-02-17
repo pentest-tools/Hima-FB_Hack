e7d9ff927cb04b5de892b338d0608598ac40ca93
Hima-Tarek <61111313+Hima-Tarek@users.noreply.github.com>
2020-02-17 14:45:29 +0200

    Add files via upload

--- hima.sh
+++ hima.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+normal='\e[0m'
+arr[0]='\e[1;94m' ; blue=${arr[0]}
+arr[1]='\e[1;31m' ; red=${arr[1]}
+arr[2]='\e[1;33m' ; yellow=${arr[2]}
+arr[3]='\e[1;35m' ; purp=${arr[3]}
+arr[4]='\e[1;32m' ; green=${arr[4]}
+arr[5]='\e[97m'   ; white=${arr[5]}
+grayterm='\e[1;40m'
+
+
+
+
+rand="$[ $RANDOM % 6 ]"
+color="${arr[$rand]}" 
+
+echo -e $color
+
 clear
 cd core
-python banner.py
+python2 banner.py
 
 
 read -p "Enter Your Choice: " choose
@@ -10,7 +27,7 @@ read -p "Enter Your Choice: " choose
 if [ $choose -eq 1 ]; then
 
 cd auto
-python hima.py
+python3 hima.py
 
 elif [ $choose -eq 2 ]; then
 clear
